c6930f3af41cb1f7a1f4109608d7c7e12bb0c48d
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2025-01-26 15:28:21 -0500

    Apply linting to build.sh

--- build.sh
+++ build.sh
@@ -4,23 +4,23 @@
 if ! [ -f "/in/${CUSTOM_KICKSTART}.ks" ]; then
 
 	# Include the live CD Kickstarts from the project
-	echo '%include /usr/share/rocky-kickstarts/live/9/x86_64/prod/rocky-live-base.ks' > "/in/${CUSTOM_KICKSTART}.ks"
+	echo '%include /usr/share/rocky-kickstarts/live/9/x86_64/prod/rocky-live-base.ks' >"/in/${CUSTOM_KICKSTART}.ks"
 
 	if [ "${SSH_ENABLED}" ]; then
 
 		# Enable the SSH service
-		echo 'services --disabled="" --enabled="NetworkManager,ModemManager,sshd"' >> "/in/${CUSTOM_KICKSTART}.ks"
+		echo 'services --disabled="" --enabled="NetworkManager,ModemManager,sshd"' >>"/in/${CUSTOM_KICKSTART}.ks"
 
 		if [ ! -z "${SSH_AUTHORIZED_KEY}" ]; then
 
 			# Add a step to the custom Kickstart to write the SSH key to root's authorized_keys
-			cat <<-EOF >> "/in/${CUSTOM_KICKSTART}.ks"
-			%post --nochroot
-			echo "${SSH_AUTHORIZED_KEY}" > \$INSTALL_ROOT/root/.ssh/authorized_keys
-			chown root:root \$INSTALL_ROOT/root/.ssh/authorized_keys
-			chmod u=rw,go= \$INSTALL_ROOT/root/.ssh/authorized_keys
+			cat <<-EOF >>"/in/${CUSTOM_KICKSTART}.ks"
+				%post --nochroot
+				echo "${SSH_AUTHORIZED_KEY}" > \$INSTALL_ROOT/root/.ssh/authorized_keys
+				chown root:root \$INSTALL_ROOT/root/.ssh/authorized_keys
+				chmod u=rw,go= \$INSTALL_ROOT/root/.ssh/authorized_keys
 
-			%end
+				%end
 
 			EOF
 		fi
@@ -28,42 +28,42 @@ if ! [ -f "/in/${CUSTOM_KICKSTART}.ks" ]; then
 		if [ ! -z "${SSH_KEY_URL}" ]; then
 
 			# Write a script to download the keys from URL and append them to authorized_keys
-			cat <<-EOF > /tmp/authorized-keys-from-url
-			#!/bin/sh
-			touch /root/.ssh/authorized_keys
-			chown root:root /root/.ssh/authorized_keys
-			chmod u=rw,go= /root/.ssh/authorized_keys
-			curl "${SSH_KEY_URL}" >> /root/.ssh/authorized_keys
+			cat <<-EOF >/tmp/authorized-keys-from-url
+				#!/bin/sh
+				touch /root/.ssh/authorized_keys
+				chown root:root /root/.ssh/authorized_keys
+				chmod u=rw,go= /root/.ssh/authorized_keys
+				curl "${SSH_KEY_URL}" >> /root/.ssh/authorized_keys
 			EOF
-			
-			# Write a service to run the keys script
-			cat <<-EOF >> /tmp/authorized-keys-from-url.service
-			[Unit]
-			Description=Download SSH public keys from URL
-			After=network-online.target
-			Wants=network-online.target
-			
-			[Service]
-			Type=oneshot
-			ExecStart=/usr/local/bin/authorized-keys-from-url
-			RemainAfterExit=true
 
-			[Install]
-			WantedBy=multi-user.target
+			# Write a service to run the keys script
+			cat <<-EOF >>/tmp/authorized-keys-from-url.service
+				[Unit]
+				Description=Download SSH public keys from URL
+				After=network-online.target
+				Wants=network-online.target
+
+				[Service]
+				Type=oneshot
+				ExecStart=/usr/local/bin/authorized-keys-from-url
+				RemainAfterExit=true
+
+				[Install]
+				WantedBy=multi-user.target
 			EOF
 
 			# Add tasks to the custom kickstart to copy the script and service into the image and enable the service at startup
-			cat <<-EOF >> "/in/${CUSTOM_KICKSTART}.ks"
-			%post --nochroot
-			cp /tmp/authorized-keys-from-url \$INSTALL_ROOT/usr/local/bin
-			cp /tmp/authorized-keys-from-url.service \$INSTALL_ROOT/etc/systemd/system
+			cat <<-EOF >>"/in/${CUSTOM_KICKSTART}.ks"
+				%post --nochroot
+				cp /tmp/authorized-keys-from-url \$INSTALL_ROOT/usr/local/bin
+				cp /tmp/authorized-keys-from-url.service \$INSTALL_ROOT/etc/systemd/system
 
-			%end
+				%end
 
-			%post
-			systemctl enable authorized-keys-from-url.service
+				%post
+				systemctl enable authorized-keys-from-url.service
 
-			%end
+				%end
 
 			EOF
 		fi
