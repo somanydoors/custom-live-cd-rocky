c24fd992acee85c8ce547091bec05c950ca0928c
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2025-01-26 13:18:16 -0500

    Split `livecd-creator` argument onto separate line

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ ksflatten \
     -c /in/custom.ks \
     -o "/out/${FLATTENED_KICKSTART}.ks"
 
-livecd-creator --verbose \
+livecd-creator \
+    --verbose \
     --config="/out/${FLATTENED_KICKSTART}.ks" \
     --fslabel="${CD_LABEL}" \
     --cache=/var/cache/live
